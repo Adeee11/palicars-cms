f220e06f4c6199064b40d8a446db58113c74bf0a
“adeee11” <hello@iwebcode.design>
2023-01-10 15:50:35 +0530

    fixed images not uploading on strapi

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: "dktgmpwym",
+        api_key: "548181874258279",
+        api_secret: "nePc2hv-5bnSvK2QWWQ-QA0ES_g",
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
